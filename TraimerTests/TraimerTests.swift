f0a2c9a04cccc75376c4d8a0b881f671a1db89a6
BRobat <Patogeen@icloud.com>
2018-04-27 12:08:32 +0200

    Initial Commit

---
+++ TraimerTests/TraimerTests.swift
@@ -0,0 +1,36 @@
+//
+//  TraimerTests.swift
+//  TraimerTests
+//
+//  Created by Tohil on 27/04/2018.
+//  Copyright © 2018 Robat. All rights reserved.
+//
+
+import XCTest
+@testable import Traimer
+
+class TraimerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
